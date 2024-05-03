1d3126d1a1fd23c586e91f26e637889546e02987
ant1234 <antonyvidetta1986@gmail.com>
2024-05-03 16:17:47 +1200

    add speed controller to launch file

--- src/bumperbot_controller/bumperbot_controller/simple_controller.py
+++ src/bumperbot_controller/bumperbot_controller/simple_controller.py
@@ -11,19 +11,19 @@ class SimpleController(Node):
         super().__init__("simple_controller")
 
         self.declare_parameter("wheel_radius", 0.033)
-        self.declare_parameter("wheel_seperation", 0.17)
+        self.declare_parameter("wheel_separation", 0.17)
 
         self.wheel_radius = self.get_parameter("wheel_radius").get_parameter_value().double_value
-        self.wheel_seperation = self.get_parameter("wheel_seperation").get_parameter_value().double_value
+        self.wheel_separation = self.get_parameter("wheel_separation").get_parameter_value().double_value
 
         self.get_logger().info("Using wheel radius %f" % self.wheel_radius)
-        self.get_logger().info("Using wheel seperation %f" % self.wheel_seperation)
+        self.get_logger().info("Using wheel seperation %f" % self.wheel_separation)
 
         self.wheel_cmd_pub = self.create_publisher(Float64MultiArray, "simple_velocity_controller/commands", 10)
         self.vel_sub = self.create_subscription(TwistStamped, "bumperbot_controller/cmd_vel", self.velCallback, 10)
 
         self.speed_conversion_ = np.array([[self.wheel_radius / 2, self.wheel_radius / 2],
-                                           [self.wheel_radius / self.wheel_seperation, -self.wheel_radius / self.wheel_seperation]])
+                                           [self.wheel_radius / self.wheel_separation, -self.wheel_radius / self.wheel_separation]])
         
         self.get_logger().info("The conversion matrix is %s" % self.speed_conversion_)
         
--- src/bumperbot_controller/src/simple_controller.cpp
+++ src/bumperbot_controller/src/simple_controller.cpp
@@ -5,7 +5,7 @@ using std::placeholders::_1;
 SimpleController::SimpleController(const std::string & name) : Node(name) 
 {
     declare_parameter("wheel_radius", 0.033);
-    declare_parameter("wheel_seperation", 0.17);
+    declare_parameter("wheel_separation", 0.17);
 
     wheel_radius_ = get_parameter("wheel_radius").as_double();
     wheel_separation_ = get_parameter("wheel_separation").as_double();
