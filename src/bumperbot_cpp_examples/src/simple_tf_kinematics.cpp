568c3702e7aee2b28de59ecdcdb0856f898a271c
ant1234 <antonyvidetta1986@gmail.com>
2024-05-08 17:52:55 +1200

    add c++ support for broadcasting tf messages

--- src/bumperbot_cpp_examples/src/simple_tf_kinematics.cpp
+++ src/bumperbot_cpp_examples/src/simple_tf_kinematics.cpp
@@ -1,8 +1,12 @@
 #include "bumperbot_cpp_examples/simple_tf_kinematics.hpp"
 
-SimpleTfKinematics::SimpleTfKinematics(const std::string &name) : Node(name)
+using namespace std::chrono_literals;
+
+SimpleTfKinematics::SimpleTfKinematics(const std::string &name) : Node(name), x_increment_(0.05), last_x_(0.0)
 {
     static_transform_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+    dynamic_tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
     static_transform_stamped_.header.stamp = get_clock()->now();
     static_transform_stamped_.header.frame_id = "bumperbot_base";
     static_transform_stamped_.child_frame_id = "bumperbot_top";
@@ -19,7 +23,28 @@ SimpleTfKinematics::SimpleTfKinematics(const std::string &name) : Node(name)
     RCLCPP_INFO_STREAM(get_logger(), "Publishing static transform between " << 
                                       static_transform_stamped_.header.frame_id << 
                                       " and " << static_transform_stamped_.child_frame_id);
+
+    timer_ = create_wall_timer(0.1s, std::bind(&SimpleTfKinematics::timerCallback, this));
 }
+
+void SimpleTfKinematics::timerCallback()
+{
+    dynamic_transform_stamped_.header.stamp = get_clock()->now();
+    dynamic_transform_stamped_.header.frame_id = "odom";
+    dynamic_transform_stamped_.child_frame_id = "bumperbot_base";
+    dynamic_transform_stamped_.transform.translation.x = last_x_ + x_increment_;
+    dynamic_transform_stamped_.transform.translation.y = 0.0;
+    dynamic_transform_stamped_.transform.translation.z = 0.0;
+    dynamic_transform_stamped_.transform.rotation.x = 0.0;
+    dynamic_transform_stamped_.transform.rotation.y = 0.0;
+    dynamic_transform_stamped_.transform.rotation.z = 0.0;
+    dynamic_transform_stamped_.transform.rotation.w = 1.0;
+
+    dynamic_tf_broadcaster_->sendTransform(dynamic_transform_stamped_);
+
+    last_x_ = dynamic_transform_stamped_.transform.translation.x;
+}
+
 
 int main(int argc, char* argv[])
 {
