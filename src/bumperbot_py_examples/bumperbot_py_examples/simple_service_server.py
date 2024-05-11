04176c9c6b446b625cb9de97c0a33a56dd1396bd
ant1234 <antonyvidetta1986@gmail.com>
2024-05-11 13:40:09 +1200

    fix typo

--- src/bumperbot_py_examples/bumperbot_py_examples/simple_service_server.py
+++ src/bumperbot_py_examples/bumperbot_py_examples/simple_service_server.py
@@ -6,7 +6,7 @@ class SimpleServiceServer(Node):
     def __init__(self):
         super().__init__("simple_service_server")
 
-        self.server = self.create_service(AddTwoInts, "add_two_ints", self.serviceCallback)
+        self.service_ = self.create_service(AddTwoInts, "add_two_ints", self.serviceCallback)
 
         self.get_logger().info("Service add_two_ints ready")
     
@@ -25,5 +25,5 @@ def main():
     simple_service_server.destroy_node()
     rclpy.shutdown()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
