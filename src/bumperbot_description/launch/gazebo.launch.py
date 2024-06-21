86855c9f2bfa465a6c7486873c3483f396e14b3d
ant1234 <antonyvidetta1986@gmail.com>
2024-06-21 16:38:26 +1200

    add worlds and models and configure them in the launch file

--- src/bumperbot_description/launch/gazebo.launch.py
+++ src/bumperbot_description/launch/gazebo.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory, get_package
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -22,6 +22,14 @@ def generate_launch_description():
                                       description="Absolute path to robot urdf file"
     )
 
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+
+    world_path = PathJoinSubstitution([
+        bumperbot_description,
+        "worlds",
+        PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
+    ])
+
     model_path = os.path.join(bumperbot_description, "models")
     model_path += pathsep + os.path.join(bumperbot_description_prefix, "share")
 
@@ -33,13 +41,17 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}]
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
     )
 
     start_gazebo_server = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros_dir, "launch", "gzserver.launch.py")
-        )
+        ),
+        launch_arguments={
+            "world": world_path
+        }.items(),
     )
 
     start_gazebo_client = IncludeLaunchDescription(
@@ -58,6 +70,7 @@ def generate_launch_description():
     return LaunchDescription([
         env_var,
         model_arg,
+        world_name_arg,
         start_gazebo_server,
         start_gazebo_client,
         robot_state_publisher_node,
