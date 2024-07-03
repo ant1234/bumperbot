2bc0943cace6caf10f955e757614454fab758de2
ant1234 <antonyvidetta1986@gmail.com>
2024-07-03 18:25:08 +1200

    odom model c++ support

---
+++ src/bumperbot_localization/include/bumperbot_localization/odometry_motion_model.hpp
@@ -0,0 +1,31 @@
+#ifndef ODOMOTRY_MOTION_MODEL_HPP
+#define ODOMOTRY_MOTION_MODEL_HPP
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
+
+class OdometryMotionModel : public rclcpp::Node
+{
+    public:
+        OdometryMotionModel(const std::string & name);
+    
+    private:
+        void odomCallback(const nav_msgs::msg::Odometry &);
+
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+        rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr pose_array_pub_;
+
+        geometry_msgs::msg::PoseArray samples_;
+        double alpha1_;
+        double alpha1_;
+        double alpha1_;
+        double alpha1_;
+        int nr_samples_;
+
+        double last_odom_x_;
+        double last_odom_y_;
+        double last_odom_theta_;
+        bool is_first_odom_;
+};
+
+#endif
