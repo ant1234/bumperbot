c70bf6af6c0baebe6e9f2bafb60155a3e888348c
ant1234 <antonyvidetta1986@gmail.com>
2024-07-16 17:15:41 +1200

    configure twist_mux control

--- src/bumperbot_controller/launch/joystick_teleop.launch.py
+++ src/bumperbot_controller/launch/joystick_teleop.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
 
 def generate_launch_description():
 
@@ -17,7 +18,25 @@ def generate_launch_description():
         executable="joy_teleop",
         parameters=[os.path.join(get_package_share_directory("bumperbot_controller"), "config", "joy_teleop.yaml")]
     )
+
+    bumperbot_controller_pkg = get_package_share_directory("bumperbot_controller")
+
+    twist_mux_launch = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("twist_mux"),
+            "launch",
+            "twist_mux_launch.py"
+        ),
+        launch_arguments={
+            "cmd_vel_out": "bumperbot_controller/cmd_vel_unstamped",
+            "config_topics": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_topics.yaml"),
+            "config_locks": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_locks.yaml"),
+            "config_joy": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_joy.yaml"),
+        }.items()
+    )
+
     return LaunchDescription([
         joy_node,
-        joy_teleop
+        joy_teleop, 
+        twist_mux_launch
     ])
